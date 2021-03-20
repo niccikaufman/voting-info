59883697ac435d16eb3e43e82ab7060409eeb629
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-20 12:12:54 -0700

    add landing page image which prompts user to enter their age when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
-  const age = parseInt(prompt("How old are you?"));
-
+  $("img#landing-page").click(function() {
+  const age = parseInt(prompt("How old are you"));
+  
   if (age >= 18) {
     $('#over-18').show();
+    $('#landing-page').toggle();
   } if (age === 17) {
     $('#register-17').show();
-  } if (age < 17) {
+    $('#landing-page').toggle();
+  } if (age === 16) {
+    $('#register-17').show();
+    $('#landing-page').toggle();
+  } if (age <= 15) {
     $('#under-18').show();
+    $('#landing-page').toggle();
   }
+  })  
 });
